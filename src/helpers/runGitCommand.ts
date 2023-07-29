28f0b2b41152a6dea1b481a67b380d7d9bc87410
safdar-azeem <safdarazeem00@gmail.com>
2023-07-29 18:39:56 +0500

    Update runGigtCommand.ts

--- src/helpers/runGitCommand.ts
+++ src/helpers/runGitCommand.ts
@@ -4,11 +4,7 @@ export const runGitCommand = (command: string, args: string[]): Promise<void> =>
     return new Promise((resolve, reject) => {
         const child: ChildProcess = spawn(command, args);
         child.on('close', (code: number) => {
-            if (code !== 0) {
-                reject(new Error(`Command ${command} failed`));
-            } else {
-                resolve();
-            }
+            resolve();
         });
     });
 };
